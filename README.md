9120393d96468624a5f36c6e7c3a34a71d1f8395
bglamadrid <bg.lamadrid@gmail.com>
2022-03-14 16:25:00 -0300

    feat: add color palette page, script & stylesheet

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Named Colors
 
+Forked from [ekelseya/colorNames](https://github.com/ekelseya/colorNames).
+
+## Language/Tools used
+
+### colors.html
+
 A simple webpage written in HTML5 and CSS to showcase the 140 named colors supported by modern browsers.
-
-## Languages
-
 *   HTML
 *   CSS
 *   Bootstrap 4
 
+### palette.html
+
+A small webpage that dynamically fetches all available colors, arranges them, and allows to copy hex codes by click
+*   HTML
+*   CSS
+*   [RxJS 7](https://github.com/ReactiveX/rxjs)
+*   [Tippy.js](https://github.com/atomiks/tippyjs)
---
+++ palette.js
@@ -0,0 +1,38 @@
+const { concat, from } = rxjs;
+const { ajax } = rxjs.ajax;
+const { finalize, switchMap, tap } = rxjs.operators;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const colorContainers = document.querySelectorAll('main .colors > div');
+
+  ajax.getJSON(`/color-rainbow.json`).pipe(
+    switchMap(rootNode => from(rootNode.colors)), // iterate through colors
+    tap(colorMetadata => {
+      colorContainers.forEach(c => {
+        const colorDiv = document.createElement('div');
+        colorDiv.classList.add('individual', 'color');
+        colorDiv.dataset.rgb = colorMetadata['rgb'];
+        colorDiv.dataset.hex = colorMetadata['hex'];
+        colorDiv.style.backgroundColor = colorMetadata['html-name'];
+        tippy(
+          colorDiv,
+          {
+            content: `${colorMetadata.name} (${colorMetadata['html-name']})</br>${colorMetadata.hex}</br>${colorMetadata.rgb}</br><b>Click to copy HEX color</b>`,
+            allowHTML: true,
+            placement: 'bottom'
+          }
+        );
+        c.appendChild(colorDiv)
+      });
+    }),
+    finalize(() => {
+      colorContainers.forEach(c => {
+        c.addEventListener('click', (event) => {
+          if (event.target.classList.contains('individual', 'color')) {
+            navigator.clipboard.writeText(event.target.dataset.hex);
+          }
+        });
+      })
+    })
+  ).subscribe();
+});
